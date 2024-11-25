c5eb866f22cde2cbd383aa7b12993d035e7f125b
SULDev2024 <samiullahlalee@gmail.com>
2024-11-25 18:02:07 +0600

    Add files via upload

---
+++ For jenkins test/add.java
@@ -0,0 +1,10 @@
+public class add {
+
+    public static void main(String[] args) {
+        int n1 = 4;
+        int n2 = 5;
+        int res=n1+n2;
+
+        System.out.println("The sum of the numbers are"+": "+ res);
+    }
+}
